46054286b575950efb1f4fd4767d4e8c71c4f2ef
kaylane sophia <kaylanesophia220@gmail.com>
2024-10-05 23:13:46 -0300

    Estilizando o erro e Criando a adição de uma nova task

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
-
+const taskContainer = document.querySelector(".tasks-container");
 //Validação do input
 const validateInput = () =>{
     if(inputElement.value.trim().length > 0){
@@ -9,18 +9,36 @@ const validateInput = () =>{
         return false;
     }
 }
-//Adicionar ações de erro caso o input esteja inválido
 const handleAddTask = () =>{
     const inputisValid = validateInput();
-
+    
+    //Adicionar ações de erro caso o input esteja inválido
     if(!inputisValid){
         return inputElement.classList.add("error");
     }
+
+    //Criando a estrutura de elementos para uma nova task pelo javascript
+    const taskItemContainer = document.createElement("div"); 
+    taskItemContainer.classList.add("task-item");
+
+    const textContent = document.createElement("p");
+    textContent.innerText = inputElement.value;
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("fa-solid");
+    deleteItem.classList.add("fa-trash-can");
+
+    //Add cada elemento na caixa de um item para formar a estrutura
+    taskItemContainer.appendChild(textContent);
+    taskItemContainer.appendChild(deleteItem);
+    //Add cada nova task para a caixa dos items
+    taskContainer.appendChild(taskItemContainer);
+
 }
-//Retirar as ações de erro caso o input seja corrigido 
 const handleInputChange = () => {
     const inputisValid = validateInput();
-
+    
+    //Retirar as ações de erro caso o input seja corrigido 
     if(inputisValid){
         return inputElement.classList.remove("error");
     }
